2c1ec2861619621071a28070f2cf34800d6b2677
RishabKumar <rkmr039@gmail.com>
2018-03-29 18:17:27 +0530

    Carsoul using HTML5, CSS3 and JavaScript

---
+++ slideshow.js
@@ -0,0 +1,41 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function SwapSlides(n)
+{
+	showSlides(slideIndex += n);
+}
+
+function currentSlide()
+{
+	showSlides(slideIndex = n);
+}
+/*
+// button based slideshow
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("MySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}*/
+//auto matic slideshow
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("MySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
